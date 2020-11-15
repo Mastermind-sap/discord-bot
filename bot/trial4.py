0aa9dc2949a8cd0f6a7e6f7711999f77e64c520f
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2020-11-16 01:57:27 +0530

    Added a custom help using embed
    Moreover removed is_it_me check from most commands such that anyone can use the command and not only the user who is running the bot.
    We need to keep that check in more sensitive functions like kick or banning a person in a server such that everyone cannot do that.

--- bot/trial4.py
+++ bot/trial4.py
@@ -9,9 +9,32 @@ token = open("token.txt", "r").read()
 mainaccid=open("mainaccid.txt", "r").read()
 
 bot = commands.Bot(command_prefix='!joker ')
+bot.remove_command("help")
 status=cycle(["Why So Sad!?","JOKER IS HERE","Use !joker"])
 
-
+@bot.command(pass_context=True)
+async def help(ctx):
+    author=ctx.message.author
+    help_embed=discord.Embed(title="Joker Help",description="Use !joker before any command",color=discord.Colour.red())
+    help_embed.set_thumbnail(url=bot.user.avatar_url)
+    help_embed.add_field(name="mention",value="mentions a user",inline=False)
+    help_embed.add_field(name="whois/info/details",value="returns details of mentioned user",inline=False)
+    help_embed.add_field(name="getdp/getprofilepic/dp",value="returns the profile pic of mentioned user",inline=False)
+    help_embed.add_field(name="pm/dm/pvtmessage",value="sends private message to mentioned user",inline=False)
+    help_embed.add_field(name="clean/clear",value="one with managing messages permission can delete n number of msgs from a channel",inline=False)
+    help_embed.add_field(name="hi/hello",value="say hi",inline=False)
+    help_embed.add_field(name="uthere/areuthere",value="ask are you there",inline=False)
+    help_embed.add_field(name="insult",value="insult someone",inline=False)
+    help_embed.add_field(name="goodnight/goodn8/nightynight",value="wish goodnight",inline=False)
+    help_embed.add_field(name="bye/sayonara/adios",value="say bye",inline=False)
+    help_embed.add_field(name="spam",value="spam some text n number of times",inline=False)
+    help_embed.add_field(name="choice/choose/select/random",value="choose random item from a list",inline=False)
+    help_embed.add_field(name="ping",value="display the ping",inline=False)
+    help_embed.add_field(name="cjoke/coding_joke",value="crack a coding related joke",inline=False)
+    help_embed.add_field(name="joke",value="crack a joke",inline=False)
+    help_embed.set_footer(text="Requested by: "+str(author))
+    await author.send(embed=help_embed)
+    
 def is_it_me(ctx):
     return str(ctx.author.id) == mainaccid
 
@@ -38,7 +61,6 @@ async def on_command_error(ctx,error):
         await ctx.send("Invalid command!")
 
 @bot.command()
-@commands.check(is_it_me)
 async def mention(ctx, user : discord.Member):
   await ctx.send(user.mention)
 
@@ -51,10 +73,7 @@ async def whois(ctx, member : discord.Member = None):
     embed.set_thumbnail(url=member.avatar_url)
     await ctx.send(embed=embed)
 
-
-
 @bot.command(aliases=["getprofilepic","dp"])
-@commands.check(is_it_me)
 async def getdp(ctx, member: discord.Member = None):
     if not member:
         member = ctx.author
@@ -79,53 +98,43 @@ async def clear(ctx,amount=1):
     await ctx.channel.purge(limit=amount+1)
 
 @bot.command(aliases=["hi"])
-@commands.check(is_it_me)
 async def hello(ctx, user : discord.Member):
      await ctx.send('Hello! '+user.mention, file=discord.File('hello.gif'))
 
 @bot.command(aliases=["uthere"])
-@commands.check(is_it_me)
 async def areuthere(ctx, user : discord.Member):
      await ctx.send('Are You There? '+user.mention, file=discord.File('areuthere'+str(random.randint(1,3))+'.gif'))
 
 @bot.command()
-@commands.check(is_it_me)
 async def insult(ctx, user : discord.Member):
     await ctx.send('Insulting'+user.mention,file=discord.File('insult'+str(random.randint(1,4))+'.gif'))
 
 @bot.command(aliases=["goodn8","nightynight"])
-@commands.check(is_it_me)
 async def goodnight(ctx, user : discord.Member):
     await ctx.send('Good Night! '+user.mention,file=discord.File('gn.gif'))
 
 @bot.command(aliases=["sayonara","adios"])
-@commands.check(is_it_me)
 async def bye(ctx, user : discord.Member):
     await ctx.send('Bye! '+user.mention,file=discord.File('bye.gif'))
 
 @bot.command()
-@commands.check(is_it_me)
 async def spam(ctx, times,*,text):
     for i in range(int(times)):
         await ctx.send(text)
 
 @bot.command(aliases=["choose","select","random"])
-@commands.check(is_it_me)
 async def choice(ctx,options="yes ,no"):
     await ctx.send(random.choice(options.split(",")))
 
 @bot.command()
-@commands.check(is_it_me)
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency *1000)}ms')
 
 @bot.command(aliases=["cjoke"])
-@commands.check(is_it_me)
 async def coding_joke(ctx):
     await ctx.send(pyjokes.get_joke())
 
 @bot.command()
-@commands.check(is_it_me)
 async def joke(ctx):
     await ctx.send(joke_generator.generate())
 

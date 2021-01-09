7afe6c44d5e48acb707ef46af62e4c4ffb4c7ffe
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2021-01-09 17:07:01 +0530

    Huge update
    New features:
    -Can be in two different servers(not channels)
    -Sends a embed with info related to the yt video
    -some good interface with emojis
    Some checks still remain!!!!

--- bot/audio_trial3.py
+++ bot/audio_trial3.py
@@ -15,17 +15,24 @@ async def on_ready():
 
 @bot.command()
 async def play(ctx, url : str):
-    song_there = os.path.isfile(r"C:\Users\user\Desktop\discord bot\song.mp4")
+##    song_there = os.path.isfile(r"C:\Users\user\Desktop\discord bot\song.mp4")
+##    try:
+##        if song_there:
+##            os.remove(r"C:\Users\user\Desktop\discord bot\song.mp4")
+##    except PermissionError:
+##        await ctx.send("Wait for the current playing music to end or use the 'stop' command :negative_squared_cross_mark:")
+##        return
     try:
-        if song_there:
-            os.remove(r"C:\Users\user\Desktop\discord bot\song.mp4")
-    except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        voiceChannel = discord.utils.get(ctx.guild.voice_channels, name=str(ctx.message.author.voice.channel))
+        await voiceChannel.connect()
+        await ctx.send("Joined "+str(ctx.message.author.voice.channel)+" voice channel!:white_check_mark:")
+        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    except AttributeError:
+        await ctx.send(ctx.message.author.mention+" is not in any voice channel :negative_squared_cross_mark:")
         return
-
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
-    await voiceChannel.connect()
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    except:
+        await ctx.send(ctx.message.author.mention+" joker in another voice channel of this server \n Cannot be in two voice channels simultaneously :negative_squared_cross_mark:")
+    
 ##    ydl_opts = {
 ##        'format': 'bestaudio/best',
 ##        'postprocessors': [{
@@ -37,22 +44,30 @@ async def play(ctx, url : str):
 ##    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 ##        ydl.download([url])
     yt=YouTube(str(url))
+    yt_embed=discord.Embed(title=yt.title+":musical_note:",description=yt.description,color=discord.Colour.red())
+    yt_embed.set_thumbnail(url=yt.thumbnail_url)
+    yt_embed.add_field(name="Author: ",value=yt.author+":musical_score: ",inline=False)
+    yt_embed.add_field(name="Duration: ",value=str(yt.length)+" seconds :clock3: ",inline=False)
+    yt_embed.add_field(name="Publish date: ",value=str(yt.publish_date)+":calendar_spiral:",inline=False)
+    yt_embed.add_field(name="Rating: ",value=str(yt.rating)+":star2:",inline=False)
+    yt_embed.add_field(name="Views: ",value=str(yt.views)+":eyes:",inline=False)
     t=yt.streams.filter(only_audio=True)
     t[0].download(r"C:\Users\user\Desktop\discord bot")
-    await ctx.send("Playing : "+str(url))
-    for file in os.listdir("./"):
-        if file.endswith(".mp4"):
-            os.rename(file, r"C:\Users\user\Desktop\discord bot\song.mp4")
-    voice.play(discord.FFmpegPCMAudio("song.mp4"))
-    
+##    for file in os.listdir("./"):
+##        if file.endswith(".mp4"):
+##            os.rename(file, r"C:\Users\user\Desktop\discord bot\song.mp4")
+    voice.play(discord.FFmpegPCMAudio(yt.title+".mp4"))
+    await ctx.send("Playing "+yt.title+" :loud_sound:")
+    await ctx.send(embed=yt_embed)
 
-@bot.command()
+@bot.command(aliases=["disconnect","exit"])
 async def leave(ctx):
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     if voice.is_connected():
         await voice.disconnect()
+        await ctx.send("Disconnected :wave:")
     else:
-        await ctx.send("The bot is not connected to a voice channel.")
+        await ctx.send("The bot is not connected to a voice channel. :negative_squared_cross_mark:")
 
 
 @bot.command()
@@ -60,8 +75,9 @@ async def pause(ctx):
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.pause()
+        await ctx.send("Paused :pause_button:")
     else:
-        await ctx.send("Currently no audio is playing.")
+        await ctx.send("Currently no audio is playing. :negative_squared_cross_mark:")
 
 
 @bot.command()
@@ -69,14 +85,16 @@ async def resume(ctx):
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     if voice.is_paused():
         voice.resume()
+        await ctx.send("Resumed :play_pause: ")
     else:
-        await ctx.send("The audio is not paused.")
+        await ctx.send("The audio is not paused. :negative_squared_cross_mark:")
 
 
 @bot.command()
 async def stop(ctx):
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     voice.stop()
+    await ctx.send("Stopped playing :octagonal_sign: ")
 
 
 bot.run(token)
